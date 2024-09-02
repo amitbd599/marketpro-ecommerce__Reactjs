f64223452fc88e7246b993a97b2ba53958c36490
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-09-02 21:11:41 +0600

    DealsOne add

--- master/src/pages/HomePageTwo.jsx
+++ master/src/pages/HomePageTwo.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Preloader from "../helper/Preloader";
 import HeaderTwo from "../components/HeaderTwo";
 import BannerTwo from "../components/BannerTwo";
+import PromotionalTwo from "../components/PromotionalTwo";
+import DealsOne from "../components/DealsOne";
 
 const HomePageTwo = () => {
 
@@ -18,7 +20,11 @@ const HomePageTwo = () => {
       {/* BannerTwo */}
       <BannerTwo />
 
+      {/* PromotionalTwo */}
+      <PromotionalTwo />
 
+      {/* DealsOne */}
+      <DealsOne />
 
 
     </>
