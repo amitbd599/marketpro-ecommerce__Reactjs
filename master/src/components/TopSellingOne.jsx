0edf775f007b905dc8904a91dbd21514e720a13c
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-09-05 23:49:37 +0600

    Update TopSellingOne.jsx

--- master/src/components/TopSellingOne.jsx
+++ master/src/components/TopSellingOne.jsx
@@ -40,36 +40,27 @@ const TopSellingOne = () => {
         prevArrow: <SamplePrevArrow />,
         responsive: [
             {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 8,
-                    slidesToScroll: 8,
-                    infinite: true,
-                    dots: true,
-                },
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 6,
-                    slidesToScroll: 6,
-                    initialSlide: 2,
-                },
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 5,
-                    slidesToScroll: 5,
-                },
-            },
-            {
-                breakpoint: 576,
+                breakpoint: 1399,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3,
+                    arrows: false,
                 },
             },
+            {
+                breakpoint: 1199,
+                settings: {
+                    slidesToShow: 2,
+                    arrows: false,
+                },
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                },
+            },
+
         ],
     };
     return (
