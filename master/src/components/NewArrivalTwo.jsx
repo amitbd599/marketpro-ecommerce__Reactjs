d79f8bf4e2bee7e7f724990c670cbc92e6b8e10c
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-09-05 23:58:35 +0600

    Update NewArrivalTwo.jsx

--- master/src/components/NewArrivalTwo.jsx
+++ master/src/components/NewArrivalTwo.jsx
@@ -40,36 +40,41 @@ const NewArrivalTwo = () => {
         prevArrow: <SamplePrevArrow />,
         responsive: [
             {
-                breakpoint: 1024,
+                breakpoint: 1599,
                 settings: {
-                    slidesToShow: 8,
-                    slidesToScroll: 8,
-                    infinite: true,
-                    dots: true,
+                    slidesToShow: 6,
+                    arrows: false,
+                },
+            },
+            {
+                breakpoint: 1399,
+                settings: {
+                    slidesToShow: 4,
+                    arrows: false,
                 },
             },
             {
                 breakpoint: 992,
                 settings: {
-                    slidesToShow: 6,
-                    slidesToScroll: 6,
-                    initialSlide: 2,
-                },
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 5,
-                    slidesToScroll: 5,
-                },
-            },
-            {
-                breakpoint: 576,
-                settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3,
+                    arrows: false,
                 },
             },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 2,
+                    arrows: false,
+                },
+            },
+            {
+                breakpoint: 424,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                },
+            },
+
         ],
     };
     return (
