e90da2ed0cb4d84b7185fc4520da9691e42dcd6e
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-09-05 23:21:05 +0600

    Update HotDealsOne.jsx

--- master/src/components/HotDealsOne.jsx
+++ master/src/components/HotDealsOne.jsx
@@ -50,36 +50,24 @@ const HotDealsOne = () => {
         prevArrow: <SamplePrevArrow />,
         responsive: [
             {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 8,
-                    slidesToScroll: 8,
-                    infinite: true,
-                    dots: true,
-                },
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 6,
-                    slidesToScroll: 6,
-                    initialSlide: 2,
-                },
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 5,
-                    slidesToScroll: 5,
-                },
-            },
-            {
-                breakpoint: 576,
+                breakpoint: 1399,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3,
                 },
             },
+            {
+                breakpoint: 1199,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+
         ],
     };
     return (
